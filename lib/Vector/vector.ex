a2d28a6327c750c485ec9dd7e8d06cec21d3ccae
leighshepperson <pmp07ls@gmail.com>
2016-02-17 22:31:46 +0000

    Implemented dot function

--- lib/Vector/vector.ex
+++ lib/Vector/vector.ex
@@ -6,12 +6,18 @@ defmodule ExAlgebra.Vector do
 	end
 
 	def sqr_magnitude(vector) do
-		vector |> List.foldl(0, fn(element, sum_of_squares) -> element * element + sum_of_squares end)
+		vector |> dot(vector)
 	end
 
 	def normalize(vector) do
 		magnitude = magnitude(vector)
 		vector |> Enum.map(fn(element) -> element / magnitude end)
+	end
+
+	def dot([], []), do: 0
+
+	def dot([vector_one_head | vector_one_tail], [vector_two_head | vector_two_tail]) do
+		vector_one_head * vector_two_head + dot(vector_one_tail, vector_two_tail)
 	end
 
 end
