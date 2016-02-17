96af4bf4de3cb0db4c0a8f800fd138b569bfc644
leighshepperson <pmp07ls@gmail.com>
2016-02-17 22:00:36 +0000

    Added failing tests for normalise function, and included additional precision methods for testing

--- test/Vector/vector_test.exs
+++ test/Vector/vector_test.exs
@@ -9,8 +9,22 @@ defmodule ExAlgebra.VectorTest do
 		assert approximate_evaluation(magnitude(input)) == expected
 	end
 
+	test "Returns the normalized vector" do
+		input = [1, 2, 3, 4]
+		expected = [0.1825741858, 0.3651483716, 0.547722557, 0.7302967433]
+		assert approximate_evaluation(normalize(input)) == expected
+	end
+
+	defp approximate_evaluation([h|t]) do
+		[approximate_evaluation(h) | approximate_evaluation(t)]
+	end
+
 	defp approximate_evaluation(function_to_evaluate) do
 		function_to_evaluate |> approximate_evaluation(@precision)
+	end
+
+	defp approximate_evaluation([h|t], precision) do
+		[approximate_evaluation(h, precision) | approximate_evaluation(t, precision)]
 	end
 
 	defp approximate_evaluation(function_to_evaluate, precision) do
